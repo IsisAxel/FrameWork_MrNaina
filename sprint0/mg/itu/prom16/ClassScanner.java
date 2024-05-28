0337e2b5663fa7cdd2463bc5571bedc86ef32be2
IsisAxel <misisaxel@gmail.com>
2024-05-28 08:05:37 +0200

    sprint2_2442

--- sprint0/mg/itu/prom16/ClassScanner.java
+++ sprint0/mg/itu/prom16/ClassScanner.java
@@ -1,47 +1,54 @@
 package mg.itu.prom16;
-
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.lang.annotation.Annotation;
+import java.util.Map;
+import java.util.HashMap;
 
+import java.lang.reflect.Method;
 
+import mg.itu.prom16.Mapping;
+import mg.itu.prom16.Get;
 public class ClassScanner {
 
-    public static List<Class<?>> getControllers(String packageName , Class cl) throws Exception {
-
-        List<Class<?>> classes = new ArrayList<>();
-        String path = packageName.replace('.', '/');
+    public static Map<String,Mapping> getMapping(String packageName,Class<? extends Annotation> annotationClass) throws ClassNotFoundException, IOException {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        String packagePath = packageName.replace(".", "/");
+        Enumeration<URL> resources = classLoader.getResources(packagePath);
     
-        URL url = Thread.currentThread().getContextClassLoader().getResource(path);
-        if (url == null) {
-            throw new Exception("Package :" + packageName + "nom trouve");
-        }
-    
-        File directory = new File(url.toURI());
-        File[] files = directory.listFiles();
-    
-        for (File file : files) {
-            String fileName = file.getName();
-
-            if (fileName.endsWith(".class")) {
-                String className = packageName + '.' + fileName.substring(0, fileName.length() - 6);
-    
-                try {
-                    ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-                    Class<?> loadedClass = classLoader.loadClass(className);
-
-                    if (loadedClass.isAnnotationPresent(cl)) {
-                        classes.add(loadedClass);
-                    } 
-                } 
-                catch (Exception e) {
-                    e.printStackTrace();
+        Map<String,Mapping> map=new HashMap<String,Mapping>();
+        while (resources.hasMoreElements()) {
+            URL resource = resources.nextElement();
+            if (resource.getProtocol().equals("file")) {
+                File directory = new File(resource.getPath());
+                for (File file : directory.listFiles()) {
+                    if (file.isFile() && file.getName().endsWith(".class")) {
+                        String className = packageName + "." + file.getName().substring(0, file.getName().length() - 6);
+                        Class<?> class1=Class.forName(className);
+                        if (class1.isAnnotationPresent(annotationClass)) {
+                            map.putAll(getMethods(class1));                            
+                        }
+                    }
                 }
             }
         }
-        return classes;
+        return map;
+    }    
+    private static Map<String,Mapping> getMethods(Class<?> clazz) {
+        
+        Map<String,Mapping> methods = new HashMap<String,Mapping>();
+        for (Method method : clazz.getDeclaredMethods()) {
+            Get an= method.getAnnotation(Get.class);
+
+            if(an!=null&& !an.url().isEmpty()){
+                methods.put(an.url().trim(),new Mapping(clazz,method));
+            }
+        }
+        return methods;
     }
-
+    
 }
-
