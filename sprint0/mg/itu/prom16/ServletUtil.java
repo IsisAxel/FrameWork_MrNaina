aff6bc3e822bd09416d09d99f74937c53143242d
IsisAxel <misisaxel@gmail.com>
2024-07-02 07:50:05 +0200

    sprint8_2442

--- sprint0/mg/itu/prom16/ServletUtil.java
+++ sprint0/mg/itu/prom16/ServletUtil.java
@@ -24,32 +24,80 @@ public abstract class ServletUtil {
         return parameters;
     }
 
-    public static Object[] getMethodArguments(Method method, Map<String, String> params) throws Exception {
+    public static Object[] getMethodArguments(Method method, Map<String, String> params) throws IllegalArgumentException ,Exception {
         Parameter[] parameters = method.getParameters();
         Object[] arguments = new Object[parameters.length];
 
         for (int i = 0; i < parameters.length; i++) {
             Parameter parameter = parameters[i];
             ReqParam reqParam = parameter.getAnnotation(ReqParam.class);
-            String paramName = "";
-            if (reqParam == null || reqParam.value().isEmpty()) {
-                paramName = parameter.getName();
-            } else {
-                paramName = reqParam.value();
+            ReqBody reqBody = parameter.getAnnotation(ReqBody.class);
+
+
+            if(reqBody == null && reqParam == null){
+                System.out.println(reqParam);
+                System.out.println(reqBody);
+                throw new IllegalArgumentException("ETU002442 : Veuillez annoter touts les arguments de votre fonction");
             }
-
-            String paramValue = params.get(paramName);
-
- 	    if (paramValue != null) {
-                arguments[i] = TypeConverter.convert(paramValue, parameter.getType());
+            else if(reqBody != null){
+                try {                
+                    Constructor<?> constructor = parameter.getType().getDeclaredConstructor();
+                    Object obj = constructor.newInstance();
+                    
+                    for (Field field : obj.getClass().getDeclaredFields()) {
+                        String fieldName = field.getName();
+                        String paramValue = params.get(fieldName);
+                        if (paramValue != null) {
+                            field.setAccessible(true);
+                            field.set(obj, TypeConverter.convert(paramValue, field.getType()));
+                        }
+                    }
+                    arguments[i] = obj;
+                } catch (Exception e) {
+                    throw e;
+                }
             } else {
-                arguments[i] = null;
-                if (isBooleanType(parameter)) {
-                    arguments[i] = false;
+                String paramName = "";
+                if (reqParam.value().isEmpty()) {
+                    paramName = parameter.getName();
+                } else {
+                    paramName = reqParam.value();
+                }
+
+                System.out.println("Name = " + paramName);
+                String paramValue = params.get(paramName);
+
+                if (paramValue != null) {
+                    arguments[i] = TypeConverter.convert(paramValue, parameter.getType());
+                } else {
+                    arguments[i] = null;
+                    if (isBooleanType(parameter)) {
+                        arguments[i] = false;
+                    }
                 }
             }
         }
         return arguments;
+    }
+
+    public static void processSession(Object obj, HttpServletRequest request) throws Exception {
+        Class<?> clazz = obj.getClass();
+        Field[] fields = clazz.getDeclaredFields();
+
+        for (Field field : fields) {
+            if (field.getType().equals(MySession.class)) {
+                field.setAccessible(true);
+                Object sessionInstance = field.get(obj);
+                if (sessionInstance == null) {
+                    sessionInstance = MySession.class.getDeclaredConstructor().newInstance();
+                    field.set(obj, sessionInstance);
+                }
+
+                MySession session = (MySession) sessionInstance;
+                session.setSession(request.getSession());
+                break;
+            }
+        }
     }
 
     private static boolean isBooleanType(Parameter parameter) {
